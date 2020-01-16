07cdbd86f8d65b048b7efde70fe2d71b90f4b76d
devarcher <archershot@hotmail.com>
2020-01-15 18:31:53 -0600

    fixed state of fields

--- src/App.js
+++ src/App.js
@@ -11,44 +11,74 @@ class App extends React.Component {
     fields: ""
   };
 
-  handleChange = (e) => {
-    this.setState({ 
-      [e.target.name]: e.target.value });
-  }
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
-    this.setState({ fields: [...this.state.fields, this.state.fields] })
-  }
+    this.setState({
+      fields: [
+        ...this.state.fields,
+        this.state.lastName,
+        this.state.dob,
+        this.state.age,
+        this.state.phone,
+        this.state.gender
+      ]
+    });
+  };
 
   render() {
     return (
       <form action="">
-        <input type="text"
+        <input
+          type="text"
           name="lastName"
           placeholder="Last Name"
           value={this.state.lastName}
-          onChange={e => {this.handleChange(e)}}/>
-        <input type="text"
+          onChange={e => {
+            this.handleChange(e);
+          }}
+        />
+        <input
+          type="text"
           name="dob"
           placeholder="DOB"
           value={this.state.dob}
-          onChange={e => {this.handleChange(e)}}/>
-        <input type="text"
+          onChange={e => {
+            this.handleChange(e);
+          }}
+        />
+        <input
+          type="text"
           name="age"
           placeholder="Age"
           value={this.state.age}
-          onChange={e => {this.handleChange(e)}}/>
-        <input type="text"
+          onChange={e => {
+            this.handleChange(e);
+          }}
+        />
+        <input
+          type="text"
           name="phone"
           placeholder="Phone"
           value={this.state.phone}
-          onChange={e => {this.handleChange(e)}}/>
-        <input type="select"
+          onChange={e => {
+            this.handleChange(e);
+          }}
+        />
+        <input
+          type="select"
           name="gender"
           placeholder="Gender"
           value={this.state.gender}
-          onChange={e => {this.handleChange(e)}}/>
+          onChange={e => {
+            this.handleChange(e);
+          }}
+        />
         <button onClick={e => this.handleSubmit(e)}>Submit</button>
       </form>
     );
